f2c2f5b75604283efa33160f24b3dfc612cff62a
jojiiikol <jojiiikol@gmail.com>
2025-09-25 16:59:05 +0300

    автоудаление старых файлов

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import os
+from contextlib import asynccontextmanager
 
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from router.downloader_router import router as downloader_router
+from utils.scheduler import scheduler
 
 load_dotenv()
 
-app = FastAPI(title="yt-download_client")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler.start()
+    print("Scheduler started")
+    yield
+    scheduler.shutdown()
+    print("Scheduler stopped")
+app = FastAPI(title="yt-download_client", lifespan=lifespan)
 app.include_router(downloader_router)
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host=os.getenv("APP_HOST"), port=int(os.getenv("APP_PORT")))
---
+++ utils/scheduler.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from apscheduler.jobstores.memory import MemoryJobStore
+import os
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from settings import POSIX_MEDIA_DIR
+
+jobstores = {
+    "default": MemoryJobStore()
+}
+
+executors = {
+    "default": ThreadPoolExecutor(max_workers=3),
+    "processpool": ProcessPoolExecutor(max_workers=1)
+}
+
+job_defaults = {
+    "coalesce": True,
+    "max_instances": 1
+}
+
+scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults)
+
+
+def cleanup_task() -> None:
+    dir_path = POSIX_MEDIA_DIR
+    files = os.listdir(dir_path)
+    for file in files:
+        file_datetime_created = datetime.strptime(file.split(".")[0], "%Y%m%d-%H%M%S")
+        if datetime.now() - file_datetime_created > timedelta(days=1):
+            print(f"Deleting {file}")
+            os.remove(os.path.join(dir_path, file))
+
+scheduler.add_job(cleanup_task, "interval", minutes=24*60)
